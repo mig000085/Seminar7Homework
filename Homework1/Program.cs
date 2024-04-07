ba195b966cdba65b6ce90f2b45b550e4e3eb9320
ivan Mokeev <mig000085@gmail.com>
2024-04-07 18:16:57 +0400

    seminar7homework1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,22 +1,54 @@
-﻿// //Задача 1: Задайте значения M и N. 
-// // Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
-// // Использовать рекурсию, не использовать циклы.
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+// M = 8; N = 4 -> "8, 7, 6, 5, 4"
 
-Console.WriteLine("Введите неотрицательные числа m и n:");
-int m = Convert.ToInt32(Console.ReadLine());
-int n = Convert.ToInt32(Console.ReadLine());
-
-void PrintNumbers(int m, int n) // делаем вызов функции 
-                                // в которую передаем начальное и последнее числа
+void NaturalNumbersBetweenMN(int m, int n)
 {
-    if (m > n) // делаем точку остановки //  если это  цикл перенсти вниз
-               //  на две строки то не дойдем до проверки и функция не остановится и зависнет
+    if (m == n)
     {
+        Console.Write($"{m}");
         return;
     }
-    Console.Write(m + " "); // до этого вызов переменной старт
 
-    PrintNumbers(m + 1, n); //прибавляем один к стартовому числу
+    Console.Write($"{m}, ");
+    NaturalNumbersBetweenMN(m + 1, n);
 }
 
-PrintNumbers(m, n);
+void NaturalNumbersBetweenNM(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write($"{m}");
+        return;
+    }
+
+    Console.Write($"{m}, ");
+    NaturalNumbersBetweenNM(m - 1, n);
+}
+
+void Examination(int m, int n)
+{
+    if (m < n)
+    {
+        NaturalNumbersBetweenMN(m, n);
+    }
+    else if (m > n)
+    {
+        NaturalNumbersBetweenNM(m, n);
+    }
+    else
+    {
+        Console.Write(n); // or "m", becouse m = n
+    }
+}
+
+Console.WriteLine("Введите первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Examination(m, n);
